c33f55036cea953cf2dd96165623cefc25c9ab72
Eli Toffel <eli_toffel@esource.com>
2026-02-17 16:52:09 -0500

    Initial Analysis

---
+++ housekeeping.R
@@ -0,0 +1,26 @@
+###############################################
+# Housekeeping File
+# Purpose: Set up environment for project
+###############################################
+
+### 1. Load required packages
+packages <- c(
+  "tidyverse",
+  "readxl",
+  "lubridate",
+  "janitor",
+  "here"
+)
+
+# Install missing packages
+installed <- packages %in% installed.packages()[, "Package"]
+if (any(!installed)) {
+  install.packages(packages[!installed])
+}
+
+# Load packages
+lapply(packages, library, character.only = TRUE)
+
+
+### 2. Load data
+load("Q:/8020 Xcel CPP TOU/_Secure Data/Working Files/Customer survey sampling/customers.rdata")
